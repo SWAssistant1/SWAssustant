07611c46fee1234f8a3c4eb32b56e9ca75876880
SWAssistant1 <kot.janusz@intmail.pl>
2024-09-05 21:22:51 +0200

    Update uncodedeeee.js

--- uncodedeeee.js
+++ uncodedeeee.js
@@ -40,7 +40,7 @@ if (typeof GAME === 'undefined') {} else {
                 const cssres = ` #res_Panel { background: rgba(0,0,0,0.9); position: fixed; top: 450px; left: 75%; z-index: 9999; width: 200px; padding: 1px; border-radius: 5px; border-style: solid; border-width: 7px 8px 7px 7px; display:block; user-select: none; color: #333333; } #res_Panel .sekcja { position: absolute; top: -27px; left: -7px; background: rgba(0,0,0,0.9); filter: hue-rotate(196deg); background-size: 100% 100%; width: 150px; cursor: all-scroll; } #res_Panel .res_button {cursor:pointer;text-align:center; border-bottom:solid gray 1px; color: white;} #res_Panel ul {margin-left:-30px; color:white; margin:7px 0px 5px 0px; padding: 0px; text-align: center;} `;
                 const cssinne = ` #inne_Panel { background: rgba(0,0,0,0.9); position: fixed; top: 650px; left: 80%; z-index: 9999; width: 200px; padding: 1px; border-radius: 5px; border-style: solid; border-width: 7px 8px 7px 7px; display:block; user-select: none; color: #333333; } #inne_Panel .sekcja { position: absolute; top: -27px; left: -7px; background: rgba(0,0,0,0.9); filter: hue-rotate(196deg); background-size: 100% 100%; width: 150px; cursor: all-scroll; } #inne_Panel .inne_button {cursor:pointer;text-align:center; border-bottom:solid gray 1px; color: white;} #inne_Panel .pvm_killed {cursor:pointer;text-align:center; border-LGtom:solid gray 1px;text-align:center; color:white;} #inne_Panel .gamee_input{text-align:center; border-bottom:solid gray 1px; color: white;} `;
                 const html = ` <div id="main_Panel"> <div class="sekcja panel_dragg">ALL FOR ONE</div> <div class='gh_button gh_resp'>PVM<b class='gh_status red'>Off</b></div> <div class='gh_button gh_pvp'>PVP<b class='gh_status red'>Off</b></div>  <div class='gh_button gh_res'>Zbierajka<b class='gh_status red'>Off</b></div> <div class='gh_button gh_inne'>Inne<b class='gh_status red'>Off</b></div> </div> `;
-                const PVP_panel = ` <div id="pvp_Panel"> <div class="sekcja pvp_dragg">PVP</div> <div class='pvp_button pvp_pvp'>PVP<b class='pvp_status red'>Off</b></div>  <div class='pvp_button pvp_WI'>Wojny <b class='pvp_status red'>Off</b></div> <div class='pvp_button pvp_org'> wynajmij orge <b class='pvp_status red'>Off</b></div>   <div class='gameee_input'><input style='width:120px; margin-left:-2px; background:grey;text-align:center;font-size:16;' type='text' placeholder="org id" name='org_id' value='18' /></div> <div class='pvp_button pvp_WK'>Wojny Klanowe<b class='pvp_status red'>Off</b></div>  <div class='gamee_input'><input style='width:120px; margin-left:-2px; background:grey;text-align:center;font-size:16;' type='text' placeholder="Lista wojen" name='pvp_capt' value='' /></div> <div class='gameee_input'><input style='width:120px; margin-left:-2px; background:grey;text-align:center;font-size:16;' type='text' placeholder="Szybkość 10-100" name='speed_capt' value='50' /></div> </div> `;
+                const PVP_panel = ` <div id="pvp_Panel"> <div class="sekcja pvp_dragg">PVP</div> <div class='pvp_button pvp_pvp'>PVP<b class='pvp_status red'>Off</b></div>  <div class='pvp_button pvp_zmieniaj'>Zmieniaj postki <b class='pvp_status red'>Off</b></div> <div class='pvp_button pvp_WI'>Wojny <b class='pvp_status red'>Off</b></div> <div class='pvp_button pvp_org'> wynajmij orge <b class='pvp_status red'>Off</b></div>   <div class='gameee_input'><input style='width:120px; margin-left:-2px; background:grey;text-align:center;font-size:16;' type='text' placeholder="org id" name='org_id' value='18' /></div> <div class='pvp_button pvp_WK'>Wojny Klanowe<b class='pvp_status red'>Off</b></div>  <div class='gamee_input'><input style='width:120px; margin-left:-2px; background:grey;text-align:center;font-size:16;' type='text' placeholder="Lista wojen" name='pvp_capt' value='' /></div> <div class='gameee_input'><input style='width:120px; margin-left:-2px; background:grey;text-align:center;font-size:16;' type='text' placeholder="Szybkość 10-100" name='speed_capt' value='50' /></div> </div> `;
                 const RESP_panel = ` <div id="resp_Panel"> <div class="sekcja resp_dragg">SPAWN MOBKóW</div> <div class="resp_button resp_resp">On<b class="resp_status red">Off</b></div>  <div class="resp_button resp_resp1">Resp<b class="resp_status red">Off</b></div> <div class="resp_button resp_rare">exp<b class="resp_status red">Off</b></div> <div class="resp_button resp_normal">Niszczenie eq<b class="resp_status red">Off</b></div> <div class="resp_button resp_leg">Niszczenie leq<b class="resp_status red">Off</b></div> <div class="resp_button resp_blue">Ogromny ramen<b class="resp_status red">Off</b></div> <div class="resp_button resp_green">maly ramen<b class="resp_status red">Off</b></div> <div class="resp_button resp_purple">Powiekszony ramen<b class="resp_status red">Off</b></div> <div class="resp_button resp_yellow">zolta pigula<b class="resp_status red">Off</b></div> <div class="resp_button resp_red">zielona pigula<b class="resp_status red">Off</b></div> <div class="resp_button resp_magic">Czerwona pigula<b class="resp_status red">Off</b></div>    <div class="resp_button resp_on">Włącz All<b class="resp_status green">On</b></div> <div class="resp_button resp_off">Wyłącz All<b class="resp_status red">Off</b></div>   </div> `;
                 const CODE_panel = ` <div id="code_Panel"> <div class="sekcja code_dragg">Inne</div> <div class="code_button code_code">KODY<b class="code_status red">Off</b></div> <div class="code_button code_acc">Konto<b class="code_status red">Off</b></div> <div class="code_button code_zast">Zastępstwa<b class="code_status red">Off</b></div> <div class="code_button code_bh1">Błogo 250% tren<b class="code_status red">Off</b></div> <div class="code_button code_bh2">Błogo 5% kod<b class="code_status red">Off</b></div> <label class='select_input'><select id='bot_what_to_train'><option value='1'>Siła</option><option value='2'>Szybkość</option><option value='3'>Wytrzymałość</option><option value='4'>Siła Woli</option><option value='5'>Energia Ki</option><option value='6'>Wtajemniczenie</option></select></label> <label class='select_input'><select id='bot_what_to_traintime'><option value='1'>1 godz.</option><option value='2'>2 godz.</option><option value='3'>3 godz.</option><option value='4'>4 godz.</option><option value='5'>5 godz.</option><option value='6'>6 godz.</option><option value='7'>7 godz.</option><option value='8'>8 godz.</option><option value='9'>9 godz.</option><option value='10'>10 godz.</option><option value='11'>11 godz.</option><option value='12'>12 godz.</option></label> </div> `;
                 const RES_panel = ` <div id="res_Panel"> <div class="sekcja res_dragg">SUROWCE</div> <div class="res_button res_res">ZBIERAJ<b class="res_status red">Off</b></div> <div class="bt_cool" style="text-align:center; color:white;"></div> <ul></ul> </div> `;
@@ -276,6 +276,17 @@ if (typeof GAME === 'undefined') {} else {
                         PVP.wk = true;
                     }
                 });
+                
+                $('#pvp_Panel .pvp_zmieniaj').click(() => {
+                    if (PVP.zmieniaj) {
+                        $(".pvp_zmieniaj .pvp_status").removeClass("green").addClass("red").html("Off");
+                        PVP.zmieniaj = false;
+                    } else {
+                        $(".pvp_zmieniaj .pvp_status").removeClass("red").addClass("green").html("On");
+                        PVP.zmieniaj = true;
+                    }
+                });
+
                 $("#pvp_Panel input[name=pvp_capt]").val(PVP.clan_list);
                 $("#pvp_Panel input[name=speed_capt]").val(PVP.speed);
 
@@ -634,7 +645,10 @@ if (typeof GAME === 'undefined') {} else {
                 y: 1,
                 war: false,
                 buff_imp: false,
-                buff_clan: false
+                buff_clan: false,
+                chars:[],
+                start_char:0,
+                pvp_timeout:0,
             };
             PVP.checkkkk = () => {
                 let imp = $("#leader_player").find("[data-option=show_player]").attr("data-char_id");
@@ -645,6 +659,12 @@ if (typeof GAME === 'undefined') {} else {
                 let abut = $("#clan_buffs").find(`button[data-option="activate_war_buff"]`);
                 let isDisabled = $("#clan_buffs").find(`button[data-option="activate_war_buff"]`).parents("tr").hasClass("disabled");
                 PVP.emp = GAME.char_data.village_id;
+
+                for(i=0; i<GAME.player_chars; i++){
+                    char = $("li[data-option=select_char]").eq(i);
+                    PVP.chars.push(char.attr("data-char_id"));
+                }
+
                 if (GAME.quick_opts.ssj && $("#ssj_bar").css("display") === "none" && PVP.code) {
                     setTimeout(() => {
                         GAME.socket.emit('ga', {
@@ -803,9 +823,13 @@ if (typeof GAME === 'undefined') {} else {
                         PVP.caseNumber++;
                         PVP.orgi();
                         break;
-                    case 12:
+                    case 12: 
+                        PVP.caseNumber++;
+                        PVP.checkTimeout();
+                        break;
+                    case 13:
                         PVP.caseNumber = 0;
-                        PVP.go();
+                        PVP.zmien_postc();
                     default:
                 }
             };
@@ -860,6 +884,7 @@ if (typeof GAME === 'undefined') {} else {
                             char_id: $("#player_list_con .player").eq(PVP.licznik).find("[data-quick=1]").attr("data-char_id"),
                             quick: 1
                         });
+                        console.log("kill ", $("#player_list_con .player").eq(PVP.licznik).find("[data-quick=1]").attr("data-char_id"));
                         PVP.licznik++;
                         window.setTimeout(PVP.kill_players, PVP.czekajpvp / PVP.WSPP());
                     } else {
@@ -868,6 +893,7 @@ if (typeof GAME === 'undefined') {} else {
                             char_id: $("#player_list_con .player").eq(PVP.licznik).find("[data-quick=1]").attr("data-char_id"),
                             quick: 1
                         });
+                        console.log("kill ", $("#player_list_con .player").eq(PVP.licznik).find("[data-quick=1]").attr("data-char_id"));
                         PVP.licznik++;
                         window.setTimeout(PVP.kill_players, PVP.czekajpvp / PVP.WSPP());
                     }
@@ -875,8 +901,37 @@ if (typeof GAME === 'undefined') {} else {
                     window.setTimeout(PVP.start, PVP.wait_pvp / PVP.WSPP());
                     PVP.licznik = 0;
                     kom_clear();
-                }
+                } 
             };
+            PVP.checkTimeout = () => {
+
+                if (!PVP.zmieniaj) {
+                    window.setTimeout(PVP.start, PVP.wait_pvp / PVP.WSPP());
+                    return;
+                }
+                var charId = parseInt(PVP.chars);
+        
+                    var enemy = $("#player_list_con").find(".player button" + "[data-quick=1]" + ".initial_hide_forced");
+                    if ($("#player_list_con").find("[data-option=load_more_players]").length == 1) {
+                        $("#player_list_con").find("[data-option=load_more_players]").click();
+                        console.log("1");
+                        window.setTimeout(PVP.checkTimeout, PVP.czekajpvp / PVP.WSPP());
+                    }else if (enemy.length == 0) {
+                        window.setTimeout(PVP.start, PVP.wait_pvp / PVP.WSPP())
+                        console.log('2');
+                    }else if (PVP.licznik < $("#player_list_con .player").length) {
+                        if (PVP.pvp_timeout < parseInt($("#player_list_con .player").eq(PVP.licznik).find(".timer").attr("data-end")))
+                            PVP.pvp_timeout = parseInt($("#player_list_con .player").eq(PVP.licznik).find(".timer").attr("data-end"));
+                        PVP.licznik++;
+                        console.log("3 ", PVP.licznik, PVP.pvp_timeout);
+                        window.setTimeout(PVP.checkTimeout, PVP.wait_pvp / PVP.WSPP())
+                    } else {
+                        window.setTimeout(PVP.start, PVP.wait_pvp / PVP.WSPP());
+                        PVP.licznik = 0;
+                        kom_clear();
+                    }
+                
+            }
             PVP.kill_players1 = () => {
                 if (!JQS.chm.is(":focus")) {
                     var enemy = $("#player_list_con").find(".player button" + "[data-quick=1]" + ":not(.initial_hide_forced)");
@@ -896,7 +951,9 @@ if (typeof GAME === 'undefined') {} else {
                     } else if (enemy.length > 0) {
                         enemy.eq(0).click();
                         window.setTimeout(PVP.kill_players1, 110);
+                        console.log("zabijanie ", enemy.length);
                     } else {
+                        console.log("koniec wroguw", enemy.length);
                         kom_clear();
                     }
                 }
@@ -968,6 +1025,18 @@ if (typeof GAME === 'undefined') {} else {
                 });
                 window.setTimeout(PVP.teleport, 2000);
             };
+            PVP.zmien_postc = () => {
+                if (!PVP.zmieniaj) {
+                    window.setTimeout(PVP.start, PVP.wait_pvp / PVP.WSPP());
+                    return;
+                }
+
+                var charId = parseInt(PVP.chars);
+                GAME.emitOrder({ a: 2, char_id: charId });
+                PVP.chars.shift();
+
+                window.setTimeout(PVP.start, 5000);
+            }
             PVP.go = () => {
                 var x = GAME.char_data.x;
                 var y = GAME.char_data.y;
@@ -1143,9 +1212,7 @@ if (typeof GAME === 'undefined') {} else {
                     window.setTimeout(PVP.start, PVP.wait_pvp / PVP.WSPP());
                     return;
                 }
-                if (PVP.org_cnt > 9)
-                    PVP.org_cnt = 0;
-                PVP.org = false;
+
                 var org_id = 18;
                 if ($("#pvp_Panel input[name=org_id]").val() != '')
                     org_id = $("#pvp_Panel input[name=org_id]").val();
@@ -1153,10 +1220,10 @@ if (typeof GAME === 'undefined') {} else {
                     window.warx = document.getElementsByClassName("war_win")[PVP.org_cnt].getElementsByTagName("button")[0].getAttribute("data-war");
                     GAME.emitOrder({a:50,type:13,war:warx,org:org_id});
                     PVP.org_cnt +=1;
-                    PVP.org = true;
                 }, 100);
-                
-                window.setTimeout(PVP.start, PVP.wait_pvp / PVP.WSPP());
+                if (PVP.org_cnt > 8)
+                    PVP.org_cnt = 0;
+                window.setTimeout(PVP.start, 300);
             }
             PVP.dec_wars = () => {
                 console.log("dec wars");
